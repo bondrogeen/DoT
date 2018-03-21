1fb370076c4cc5c7981c3587bbcb9de0cc1a0073
Roman <bondrogeen@gmail.com>
2018-03-21 22:32:29 +0300

    fix http get

--- files/web_get.lua
+++ files/web_get.lua
@@ -20,7 +20,7 @@ local function get(t, cb)
  payload = nil
  collectgarbage()
  end)
- con:on("disconnection", function(con)file.close()cb(false)con = nil end)
+ con:on("disconnection", function(con)file.close()cb(false)end)
  con:on("connection", function(con)
   con:send("GET /"..t.path..t.file.." HTTP/1.0\r\n"..
   "Host: "..t.host.."\r\n"..
@@ -47,7 +47,7 @@ local function upload(t,cb)
   local next
   local function sf(t)
    get(t, function(d)
-    if d then cb(d) buf=buf..d..'\n' else next() end
+    if d then buf=buf..d..'\n' else next() end
    end)
   end
   next = function()
@@ -57,8 +57,7 @@ local function upload(t,cb)
     sf(t)
     tab[#tab]=nil
    else
-    temp(buf.."close ")
-    cb("close")
+    cb(buf.."close ")
     cb(false)
    end
   end
@@ -66,15 +65,18 @@ local function upload(t,cb)
 end
 
 return function (t, cb)
+ local function r(d)
+  if cb then cb(d) else temp(d) end
+ end
  file.remove("temp_get.txt")
  if t.init =="upload" then
    get(t, function(f)
    if f then
     local ok, j = pcall(sjson.decode,f)
-    if ok then t.file=j upload(t,cb) else cb(false) end
+    if ok then t.file=j upload(t,r) else r(false) end
    end
    end)
  else
-  get(t,function(d) temp(d)cb(d) end)
+  get(t,r)
  end
 end
