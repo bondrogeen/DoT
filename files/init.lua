9cdb21a4ad61bd146b1490aaf892a1312bf663d5
Roman <bondrogeen@gmail.com>
2018-04-06 23:16:56 +0300

    test

--- files/init.lua
+++ files/init.lua
@@ -5,4 +5,7 @@ dofile("init_wifi.lua")(s.wifi_mode,s.wifi_id,s.wifi_pass,function(c)
  if(not srv_init)then dofile('web.lua')end
  dofile("init_settings.lua")({com="run",net=true})
 end)
-dofile("init_settings.lua")({com="run"})
+local mytimer = tmr.create()
+mytimer:register(5000, tmr.ALARM_SINGLE,
+function (t) print("Start")dofile("init_settings.lua")({com="run"})t:unregister() end)
+mytimer:start()
