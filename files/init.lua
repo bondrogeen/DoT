898994e6714fade55666e40b39180f2f5a2d49e1
bondrogeen <bondrogeen@gmail.com>
2019-02-01 15:30:39 +0300

    ...

--- files/init.lua
+++ files/init.lua
@@ -1,1 +1,1 @@
-gpio.mode(4,gpio.INPUT)_s=dofile("init_settings.lua")(gpio.read(4))node.setcpufreq(node.CPU160MHZ)dofile("init_wifi.lua")(_s)local a=tmr.create()a:register(5000,tmr.ALARM_SINGLE,function(b)print("Start")dofile("init_settings.lua")({run={ext="run"}})b:unregister()end)a:start()
+gpio.mode(4,gpio.INPUT)_s=dofile("init_settings.lua")({def=gpio.read(4)})node.setcpufreq(node.CPU160MHZ)dofile("init_wifi.lua")(_s)local a=tmr.create()a:register(5000,tmr.ALARM_SINGLE,function(b)print("Start")dofile("init_settings.lua")({run={ext="run"}})b:unregister()end)a:start()
