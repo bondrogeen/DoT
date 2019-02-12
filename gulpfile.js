a85e29a20c6d08b60c0eb88f115751295aaaedd8
bondrogeen <bondrogeen@gmail.com>
2019-02-12 12:06:41 +0300

    up gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var gulp = require('gulp'),
- watch = require('gulp-watch'),
  gutil = require('gulp-util'),
  gzip = require('gulp-gzip'),
  cssmin = require('gulp-cssmin'),
@@ -12,9 +11,6 @@ var gulp = require('gulp'),
  luaminify = require('gulp-luaminify'),
  rigger = require('gulp-rigger');
 
-
-
-
 var path = {
  build: 'files/',
  src: {
@@ -23,6 +19,7 @@ var path = {
   style: 'src/assets/css/*.css',
   img: 'src/assets/images/**/*.*',
   lua: 'src/lua/**/*.*',
+  info: 'src/assets/info/**/*.*',
   fonts: 'src/assets/fonts/**/*.*'
  },
  watch: {
@@ -31,6 +28,7 @@ var path = {
   style: 'src/assets/css/*.css',
   img: 'src/assets/images/**/*.*',
   lua: 'src/lua/**/*.*',
+  info: 'src/assets/info/**/*.*',
   fonts: 'src/assets/fonts/**/*.*'
  },
  clean: './build'
@@ -43,71 +41,47 @@ var optionsDef = {
  minifyJS:true
  };
 
-gulp.task('html:build', function () {
- gulp.src(path.src.html)
+function html () {
+ return gulp.src(path.src.html)
   .pipe(rigger())
   .pipe(htmlmin(optionsDef))
   .pipe(replace('<?lua', '\n<?lua'))
-//  .pipe(replace(/^.*/, function(val) {
-//   console.log("\n______________________________________\n");
-//   console.log(val);
-//
-//      return val;
-//    }))
   .pipe(gulp.dest(path.build));
-});
+};
 
-gulp.task('js:build', function () {
- gulp.src(path.src.js)
+function js() {
+ return gulp.src(path.src.js)
   .pipe(rigger())
   .pipe(uglify())
   .pipe(gzip())
   .pipe(gulp.dest(path.build));
-});
+};
 
-gulp.task('css:build', function () {
- gulp.src(path.src.style)
-  //        .pipe(rigger())
+function style() {
+ return gulp.src(path.src.style)
   .pipe(cssmin())
   .pipe(rename({basename: "style"}))
   .pipe(gzip())
   .pipe(gulp.dest(path.build));
-});
+};
 
-gulp.task('img:build', function () {
- gulp.src(path.src.img)
+function img() {
+ return gulp.src(path.src.img)
   .pipe(gulp.dest(path.build));
-});
+};
 
-gulp.task('lua:build', function () {
+function lua() {
     return gulp.src(path.src.lua)
         .pipe(luaminify())
         .pipe(gulp.dest(path.build));
-});
+};
 
-gulp.task('build', [
-    'html:build',
-    'lua:build',
-    'js:build',
-    'css:build',
-    'img:build'
-]);
+function watch() {
+  gulp.watch(path.watch.html, html);
+  gulp.watch(path.watch.js, js);
+  gulp.watch(path.watch.lua, lua);
+  gulp.watch(path.watch.style, style);
+}
 
-gulp.task('watch', function () {
- watch([path.watch.html], function (event, cb) {
-  gulp.start('html:build');
- })
-
- watch([path.watch.js], function (event, cb) {
-  gulp.start('js:build');
- });
-
- watch([path.watch.lua], function (event, cb) {
-  gulp.start('lua:build');
- });
-
- watch([path.watch.style], function (event, cb) {
-  gulp.start('css:build');
- });
-
-});
+gulp.task('build', gulp.parallel(html, js, style, img, lua));
+gulp.task('watch', gulp.series(watch));
